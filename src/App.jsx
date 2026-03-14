b83479e9e324bb73c25a3f59783a34a7d8dd8684
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-14 13:18:49 +0000

    Fix file naming and import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
+import Home from './pages/HomePage';
 import Island from './pages/Island';
 import Challenges from './pages/Challenges';
 import Progression from './pages/Progression';
